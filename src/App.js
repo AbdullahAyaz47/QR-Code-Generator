e6632a0dc45bab47dc02ee1b67f4c8f65572d99b
AbdullahAyaz47 <jazaajazz2@gmail.com>
2025-06-10 20:50:37 +0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,61 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import React from "react";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import movieTrailer from "movie-trailer";
 
 function App() {
-  const [temp, setTemp] = useState("");
-  const [word, setWord] = useState("");
-  const [size, setSize] = useState(400);
-  const [bgColor, setBgColor] = useState("ffffff");
-  const [qrCode, setQrCode] = useState("");
+  const [video, setVideo] = useState("inception");
+  const [videoURL, setVideoURL] = useState("https://youtu.be/sa9l-dTv9Gk");
 
-  useEffect(() => {
-    setQrCode(
-      `http://api.qrserver.com/v1/create-qr-code/?data=${word}!&size=${size}x${size}&bgcolor=${bgColor}`
-    );
-  }, [word, size, bgColor]);
-
-  const handleClick = (e) => {
-    setWord(temp);
+  const handleClick = () => {
+    movieTrailer(video).then((res) => setVideoURL(res));
   };
-
   return (
     <div className="App">
-      <h1>QR Code Generator</h1>
-      <div className="input-box">
-        <div className="gen">
-          <input
-            type="text"
-            onChange={(e) => setTemp(e.target.value)}
-            placeholder="Enter text to encode"
-          />
-          <button onClick={handleClick}>Generate</button>
-        </div>
-
-        <div className="extra">
-          <h5>Background Colour:</h5>
-          <input
-            type="color"
-            onChange={(e) => setBgColor(e.target.value.substring(1))}
-          />
-          <h5>Dimension:</h5>
-          <input
-            type="range"
-            min="200"
-            max="600"
-            value={size}
-            onChange={(e) => setSize(e.target.value)}
-          />
-        </div>
+      <div className="search-box">
+        <label> Search for any movies/shows:</label>
+        <input type="text" onChange={(e) => setVideo(e.target.value)} />
+        <button onClick={handleClick}>Search</button>
       </div>
-
-      <div className="output-box">
-        <img src={qrCode} alt="" />
-
-        <a href={qrCode} download="QRCode">
-          <button>Download</button>
-        </a>
-      </div>
+      <ReactPlayer url={videoURL} controls={true} />
     </div>
   );
 }
